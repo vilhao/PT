bd3213988b00f88340c0225536af59fd5dac677b
Vilhao <80202993+vilhao@users.noreply.github.com>
2021-06-24 17:36:21 +0100

    Add files via upload

---
+++ MetalVilhao/Gruntfile.js
@@ -0,0 +1,45 @@
+module.exports = function(grunt) {
+  'use strict';
+
+  grunt.initConfig({
+    copy: {
+      assets: {
+        expand: true,
+        src: ["**"],
+        dest: "dist/assets",
+        cwd: "game/assets/"
+      }
+    },
+    concat: {
+      application: {
+        src: ['game/lib/*.js', 'game/lib/momentjs/moment.js', 'game/init_app.js', 'game/ledges.js', 'game/models/*.js', 'game/init_game.js'],
+        dest: 'dist/app.js'
+      }
+    },
+    watch: {
+      game: {
+        files: ['game/*.js', 'game/*/*.js'],
+        tasks: ['compile'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    connect: {
+      game: {
+        options: {
+          port: 9092,
+          base: ['.', 'dist']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('compile', ['concat', 'copy:assets']);
+  grunt.registerTask('default', ['connect:game', 'compile', 'watch']);
+};
